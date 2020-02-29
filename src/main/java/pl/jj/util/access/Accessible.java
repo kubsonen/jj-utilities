aa8e4b5788e1bf0574ab477708b78efc0507f2e9
JNartowicz <nartowiczjakub@gmail.com>
2020-02-29 15:21:42 +0100

    Accessible property name info

--- src/main/java/pl/jj/util/access/Accessible.java
+++ src/main/java/pl/jj/util/access/Accessible.java
@@ -2,22 +2,28 @@ package pl.jj.util.access;
 
 import java.lang.reflect.Method;
 
-public class Accessible {
+public final class Accessible {
 
-    private Class<?>[] fieldGenericTypes;
-    private Class<?> fieldType;
-    private Method getter;
-    private Method setter;
+    private final String property;
+    private final Class<?>[] fieldGenericTypes;
+    private final Class<?> fieldType;
+    private final Method getter;
+    private final Method setter;
 
-    private Accessible(Class<?>[] fieldGenericTypes, Class<?> fieldType, Method getter, Method setter) {
+    private Accessible(String property, Class<?>[] fieldGenericTypes, Class<?> fieldType, Method getter, Method setter) {
+        this.property = property;
         this.fieldGenericTypes = fieldGenericTypes;
         this.fieldType = fieldType;
         this.getter = getter;
         this.setter = setter;
     }
 
-    static Accessible accessible(Class<?>[] fieldGenericTypes, Class<?> fieldType, Method getter, Method setter) {
-        return new Accessible(fieldGenericTypes, fieldType, getter, setter);
+    static Accessible accessible(String property, Class<?>[] fieldGenericTypes, Class<?> fieldType, Method getter, Method setter) {
+        return new Accessible(property, fieldGenericTypes, fieldType, getter, setter);
+    }
+
+    public String getProperty() {
+        return property;
     }
 
     public Class<?>[] getFieldGenericTypes() {
